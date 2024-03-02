6450b62bad11073bbc40eabb73fd12eafa52aac5
Gourav Dodia <gouravkumardodia@gmail.com>
2024-03-02 11:28:49 +0530

    abcd

--- pqr/student.py
+++ pqr/student.py
@@ -4,13 +4,10 @@ class Student(Person):
         super().__init__(name, age)
         self._roll = roll 
         self._marks = marks
-    def input(self):
-        super().input()
-        self._roll = input("Roll : ") 
-        self._marks = float(input("Marks : "))
+   
     def show(self):
         super().show()
-        print("Roll : " , self._roll)
+        print("Roll Number: " , self._roll)
         print("Marks : " , self._marks) 
     def giveExam(self):
         print("Exam Given !")         
